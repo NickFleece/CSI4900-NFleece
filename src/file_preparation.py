5b3e650944ad39967b102d224d5f7de3f5b87295
NickFleece <nebf97@gmail.com>
2019-10-15 20:00:30 -0400

    update some cleaning params

--- src/file_preparation.py
+++ src/file_preparation.py
@@ -11,20 +11,20 @@ def clean_file(fileName):
         readCSV = csv.reader(csvfile, delimiter=',')
         for row in readCSV:
             count += 1
+            if (count % 10000 == 0):
+                print(count)
             if (row[4] == 'DNS' and row[7] == 'Message is a query' ):
-                if (count % 10000 == 0):
-                    print(count)
                 row[0] = new_id
                 temp_data.append(row)
                 new_id += 1
-    with open(f"cleaned_{fileName}.csv", 'w', newline='') as writeFile:
+    with open(f"../../Cleaned_Datasets/cleaned_{fileName}.csv", 'w', newline='') as writeFile:
         writer = csv.writer(writeFile)
         writer.writerows(temp_data)
     return False
 
 def main():
     # selectedOption = input("Please enter one of these options:\n- 0: Clean data\n")
-    file_cleaned = clean_file("20160423_data")
+    clean_file("packets2")
     return None
 
 main()
