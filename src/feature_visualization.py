80e62550dbf033d635bba03167773b009c09d8ac
Nick Fleece <nebf97@gmail.com>
2019-11-07 11:22:47 -0500

    update it to use subplots

--- src/feature_visualization.py
+++ src/feature_visualization.py
@@ -12,14 +12,26 @@ def graph_feature(data, featureName):
         color.append(row['malicious'])
         count += 1
 
-    plt.scatter(x, y, c=color, s=1)
-    plt.title(featureName)
-    plt.show()
+    return [x,y,color]
+    # plt.scatter(x, y, c=color, s=1)
+    # plt.title(featureName)
+    # plt.show()
 
 data = pd.read_csv("../data/full_data_features.csv")
-# graph_feature(data, 'f_subdomain_entropy')
-# graph_feature(data, 'f_ratio_upper_case')
-# graph_feature(data, 'f_ratio_numbers')
-# graph_feature(data, 'f_subdomain_length')
-# graph_feature(data, 'f_ratio_lower_case')
-# graph_feature(data, 'f_number_subdomains')
+features = [
+    'f_subdomain_entropy',
+    'f_ratio_upper_case',
+    'f_ratio_numbers',
+    'f_subdomain_length',
+    'f_ratio_lower_case',
+    'f_number_subdomains'
+]
+x_max = 3
+y_max = 2
+fig, ax = plt.subplots(y_max, x_max)
+for feat, i in zip(features, range(0,len(features))):
+    print(f"Processing feature: {feat}, i: {i}, x: {i%x_max}, y: {i//(y_max+1)}")
+    graph_data = graph_feature(data, feat)
+    ax[(i//(y_max+1)), (i%x_max)].scatter(graph_data[0], graph_data[1], c=graph_data[2], s=1)
+    ax[(i//(y_max+1)), (i%x_max)].set_title(feat)
+plt.show()
